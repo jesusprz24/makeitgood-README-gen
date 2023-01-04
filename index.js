b2e28fbc117c52406eb96a75e7adf989a9b56c84
Jesusprz24 <perez.jesus1039@gmial.com>
2023-01-03 20:11:34 -0700

    added information to the arrays and functions in index.js

---
+++ index.js
@@ -0,0 +1,71 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
+// TODO: Create an array of questions for user input
+const questions = [{
+    type: "input",
+    message: "What is the title of the project?",
+    name: "Title"
+}, {
+    type: "input",
+    message: "What is the project about?",
+    name: "Description"
+}, {
+    type: "input",
+    message: "Table of Contents",
+    name: "Table of Contents"
+}, {
+    type: "input",
+    message: "What does the user need to install?",
+    name: "Installation"
+}, {
+    type: "input",
+    message: "How is the app used? Give instructions",
+    name: "Usage" 
+}, {
+    type: "input",
+    message: "What is the license?", 
+    name: "License"
+}, {
+    type: "input",
+    message: "Who contributed to the project",
+    name: "Contributing"
+}, {
+    type: "input",
+    message: "Will there be a test?",
+    name: "Tests"
+}, {
+    type: "input",
+    message: "Questions included",
+    name: "Questions"
+}
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName, data, function(err) {
+        console.log(fileName)
+        console.log(data)
+        if (err) {
+            return console.log(err)
+        } else {
+            console.log("success")
+        }
+    })
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.createPromptModule(questions)
+    .then(function(data) {
+        writeToFile("README.md", generatorMarkdown(data));
+        console.log(data)
+    })
+}
+
+// Function call to initialize app
+init();
